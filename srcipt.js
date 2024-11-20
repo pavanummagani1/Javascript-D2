c530aa83ce7e1a3ccab7aeeafdbee58bf342f4ed
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-20 11:09:25 +0530

    GLOBAL EXECUTION CONTEXT

---
+++ srcipt.js
@@ -0,0 +1,47 @@
+//QUESTION-1
+console.log(a);
+var a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+
+
+//QUESTION-2
+console.log(b);
+var b = 5;
+var b = 15;
+console.log(b);
+b = b + 5;
+console.log(b);
+
+
+// //Question-3
+console.log(c);
+c = 30;
+var c;
+console.log(c);
+c = c * 2;
+console.log(c);
+
+
+//Question-4
+var d;
+console.log(d);
+d = 50;
+console.log(d);
+d = d + 10;
+console.log(d);
+var d = 100;
+console.log(d);
+
+
+//Question-5
+var e = 1;
+console.log(e);
+e = e + 1;
+console.log(e);
+var e = 10;
+console.log(e);
+e = e * 2;
+console.log(e);
+
